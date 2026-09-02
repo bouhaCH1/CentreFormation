6d75d21024d24db006a84fad4ffc337d6ca2abd8
Chaaben Beha <chaabenbeha@gmail.com>
2026-09-02 15:13:40 +0100

    Clean repo: add .gitignore to ignore build directory

--- build/Desktop_Qt_6_7_3_MinGW_64_bit_Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CentreFormation_FILE C:/Users/enabo/Desktop/CentreFormation/build/Desktop_Qt_6_7_3_MinGW_64_bit_Debug/CentreFormation.exe)
-set(__QT_DEPLOY_TARGET_CentreFormation_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CentreFormation_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
